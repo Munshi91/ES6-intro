4f1a318df30931d03267a28b22e0baaec2d7e7d4
Munshi91 <54263790+Munshi91@users.noreply.github.com>
2024-05-18 22:15:35 +0600

    Arrow Function

--- es6-intro.js
+++ es6-intro.js
@@ -6,7 +6,21 @@
 
 // console.log(total);
 
-const a = 10;
-const b = 20;
-const math = `The sum of ${a} and ${b} is ${a + b}`;
-console.log(math);
+// const a = 10;
+// const b = 20;
+// const math = `The sum of ${a} and ${b} is ${a + b}`;
+// console.log(math);
+
+function add(a, b) {
+  const result = a + b;
+  return result;
+}
+
+// const sum = add(5, 9);
+// console.log(sum);
+
+// arrow function
+
+const add2 = (a, b) => a + b;
+const sum = add2(20, 30);
+console.log(sum);
