999736b327a2aa7d69a9e2974b371f5265d2de9f
Munshi91 <54263790+Munshi91@users.noreply.github.com>
2024-05-18 22:05:15 +0600

    Template String

--- es6-intro.js
+++ es6-intro.js
@@ -1,1 +1,12 @@
-console.log('Es6 Module');
+// function add(num1, num2) {
+//   const result = num1 + num2;
+//   return result;
+// }
+// const total = add(20, 45);
+
+// console.log(total);
+
+const a = 10;
+const b = 20;
+const math = `The sum of ${a} and ${b} is ${a + b}`;
+console.log(math);
