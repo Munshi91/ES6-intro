2303ce67eeadd34c4d32a882b43ec9619c8d1a9f
Munshi91 <mimunshi91@gmail.com>
2024-05-19 13:51:00 +0600

    Update es6-intro.js

--- es6-intro.js
+++ es6-intro.js
@@ -25,6 +25,18 @@
 // const sum = add2(20, 30);
 // console.log(sum);
 
-const add = (a, b) => a - b;
-const subtraction = add(30, 20);
-console.log(subtraction);
+// const add = (a, b) => a - b;
+// const subtraction = add(30, 20);
+// console.log(subtraction);
+
+const different = (x, y) => x - y;
+const multiply = (first, second, third) => first * second * third;
+
+const getAge = parson => parson.age;
+const student = { name: 'anita', age: 45 };
+const age = getAge(student);
+console.log(age);
+
+const getThird = Number => Number[1];
+const third = getThird([5, 56, 88, 6, 9]);
+console.log(third);
